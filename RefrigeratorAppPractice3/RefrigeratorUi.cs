21be9ab270318d0bb79fb3af975b9c30aa248cf1
YousufRakib <rakibkhanphc@gmail.com>
2019-10-17 02:46:26 +0600

    Assignment-11

---
+++ RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+        WeightCalculate _weightCalculate = new WeightCalculate();
+        double maxWeight;
+        int numItem;
+        double unitWeight;
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(maxWeightTakeTextBox.Text))
+                {
+                    MessageBox.Show("Please Enter vehicle refrigerator capacity");
+                    return;
+                }
+                maxWeight = Convert.ToDouble(maxWeightTakeTextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter value");
+            }
+            
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(maxWeightTakeTextBox.Text))
+                {
+                    MessageBox.Show("Please Enter vehicle refrigerator capacity");
+                    return;
+                }
+                if (maxWeight == 0)
+                {
+                    MessageBox.Show("Please save max weight");
+                    return;
+                }
+                
+                if (String.IsNullOrEmpty(itemTextBox.Text))
+                {
+                    MessageBox.Show("Please Enter number of items");
+                    return;
+                }
+                numItem = Convert.ToInt32(itemTextBox.Text);
+                if (String.IsNullOrEmpty(weightTextBox.Text))
+                {
+                    MessageBox.Show("Please Enter per item weight");
+                    return;
+                }
+                unitWeight = Convert.ToDouble(weightTextBox.Text);
+
+                double currentWeight= Convert.ToDouble(_weightCalculate.currentWeight(numItem, unitWeight));
+                double remainWeight = Convert.ToDouble(_weightCalculate.remainWeight(maxWeight, numItem, unitWeight));
+                if (currentWeight > maxWeight)
+                {
+                    MessageBox.Show("Your current weight is more than maxweight");
+                    return;
+                }
+                else
+                {
+                    currentWeightTextBox.Text = Convert.ToString(currentWeight);
+                    remainingWeightTextBox.Text = Convert.ToString(remainWeight);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please Enter value");
+            }
+            
+        }
+    }
+}
---
+++ RefrigeratorAppPractice3/WeightCalculate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    class WeightCalculate
+    {
+        public double currentWeight(int numItem,double unitWeighy)
+        {
+            double waitCalculate = numItem * unitWeighy;
+            return waitCalculate;
+        }
+        public double remainWeight(double maxWeight,int numItem, double unitWeighy)
+        {
+            double remainCalculate =(maxWeight-(numItem * unitWeighy));
+            return remainCalculate;
+        }
+    }
+}
